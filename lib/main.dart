4f0fc36c4b8b052864c3ffb2d1c30dd20f5abc1c
Taylor Simpson <tdsimpson23@gmail.com>
2020-05-20 14:14:42 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text('My First App'),
+            centerTitle: true,
+            backgroundColor: Colors.red[600],
+        ),
+        body: Center(
+            child: RaisedButton.icon(
+                onPressed: (){},
+                icon: Icon(
+                    Icons.mail
+                ),
+                label: Te xt('Mail me'),
+                color: Colors.amber,
+            ),
+            ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: (){},
+            child: Text('Click'),
+            backgroundColor: Colors.red[600],
+        ),
+    );
+  }
+}
+
