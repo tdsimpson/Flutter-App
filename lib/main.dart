ee391048ea83c51129a3d53b9956d3394b3df6bc
Taylor Simpson <tdsimpson23@gmail.com>
2020-05-20 21:45:36 -0400

    Start of Ninja App

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,91 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    home: Home(),
+    home: NinjaCard(),
 ));
 
-class Home extends StatelessWidget {
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text('My First App'),
-            centerTitle: true,
-            backgroundColor: Colors.red[600],
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Ninja ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-                padding: EdgeInsets.all(20.0),
-                color: Colors.cyan,
-                child: Text('one')
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/thumb.png'),
+                radius: 40.0,
+              ),
             ),
-            Container(
-                padding: EdgeInsets.all(30.0),
-                color: Colors.pinkAccent,
-                child: Text('two')
+            Divider(
+              height: 90,
+              color: Colors.grey[800],
             ),
-            Container(
-                padding: EdgeInsets.all(40.0),
-                color: Colors.amber,
-                child: Text('three')
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              )
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Chun-Li',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+              )
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              )
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+              )
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'chun.li@thenetninja.co.uk',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
             ),
           ],
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: (){},
-            child: Text('Click'),
-            backgroundColor: Colors.red[600],
-        ),
+        )
+      )
     );
   }
 }
-
