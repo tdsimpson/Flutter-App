601c9d2070cf65656988bdc073b455bf9c824cb7
Taylor Simpson <tdsimpson23@gmail.com>
2020-05-22 21:15:09 -0400

    Adding API call to WorldTimeAPI

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,27 +7,7 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
 
-    //Simulate network request from a username
-    String username = await Future.delayed(Duration(seconds: 3), (){
-      return 'Yoshi';
-    });
-
-    //Simulate network request to get bio of the username
-    String bio = await Future.delayed(Duration(seconds: 2), (){
-      return ('vegan, musician & egg collector');
-    });
-
-    print('$username - $bio');
-}
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('Hey there');
-  }
 
   @override
   Widget build(BuildContext context) {
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,10 +8,35 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getTime() async {
+    // make the request
+    Response response = await get('http://worldtimeapi.org/api/timezone/America/Toronto');
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    // get properties from json
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    //print(datetime);
+    //print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.subtract(Duration(hours: int.parse(offset)));
+    print(now);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading Screen'),
+      body: Text('loading screen'),
     );
   }
 }
