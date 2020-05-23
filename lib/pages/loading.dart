cfa8b5027869cde8ef5a79a42122919371e66fff
Taylor Simpson <tdsimpson23@gmail.com>
2020-05-23 12:12:05 -0400

    Adding dynamic background images based on the current time

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart;
 
 class Loading extends StatefulWidget {
   @override
@@ -15,6 +16,7 @@ class _LoadingState extends State<Loading> {
       'location': instance.location,
       'flag': instance.flag,
       'time': instance.time,
+      'isDaytime': instance.isDaytime;
     });
   }
 
@@ -27,9 +29,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('loading'),
+      backgroundColor: Colors.blue [900],
+      body: Center(
+        child: spinkit = SpinKitRotatingCircle(
+          color: Colors.white,
+          size: 50.0,
+        ),
       ),
     );
   }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -8,6 +8,7 @@ class WorldTime {
   String time; // the time in that location
   String flag; // url to an asset flag icon
   String url; // location url for api endpoint
+  bool isDaytime; // true or false if day time or not
 
   WorldTime({this.location, this.flag, this.url});
 
@@ -30,6 +31,7 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(offset)));
 
       // set the time property
+      isDaytime = now.hour > 6 && now.hours < 20 ? true : false;
       time = DateFormat.jm().format(now);
     }
     catch(e){
