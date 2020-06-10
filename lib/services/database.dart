e0d2d2d25117444d9aceafdebad49456160dac01
Taylor Simpson <tdsimpson23@gmail.com>
2020-06-10 00:59:00 -0400

    Adding a stream to get the document data and display it on screen in a list, and adding a bottom sheet to update brew setings

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
@@ -8,11 +9,27 @@ class DatabaseService {
   final CollectionReference brewCollection =
       Firestore.instance.collection('brews');
 
-  Future updateUserData(String sugars, String name, int strength) async {
+  Future<void> updateUserData(String sugars, String name, int strength) async {
     return await brewCollection.document(uid).setData({
       'sugars': sugars,
       'name': name,
       'strength': strength,
     });
+  }
+
+  // brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      //print(doc.data);
+      return Brew(
+          name: doc.data['name'] ?? '',
+          strength: doc.data['strength'] ?? 0,
+          sugars: doc.data['sugars'] ?? '0');
+    }).toList();
+  }
+
+  // get brews stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
   }
 }
